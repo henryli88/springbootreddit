4358c1937597f98556920d1d2283f95fa808d246
henryli <henrylicoder@yahoo.com>
2019-02-25 16:10:13 -0500

    Initial commit - Spring Boot Reddit Clone

---
+++ src/main/java/com/javacoder/springbootreddit/SpringbootredditApplication.java
@@ -0,0 +1,13 @@
+package com.javacoder.springbootreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootredditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootredditApplication.class, args);
+    }
+
+}
